5b074f30c1bacaaa36d677b2f9edcc2629e77ebe
Josephine <josephine.closan@yahoo.com>
2022-10-27 14:36:39 +0300

    Define JS function bingo

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function greetings(){
-    return "Hello World!"
-
+function greetings() {
+  return "Hello World!";
 }
 
 console.log(greetings());
+
+function bingo(number) {
+  if (number > 5) {
+    return "Bingo!";
+  } else {
+    return "Try again!";
+  }
+}
+
+console.log(bingo(5));
+console.log(bingo(20));
+console.log(bingo(-1));
